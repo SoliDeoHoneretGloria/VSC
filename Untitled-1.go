8181b138e5d74e320e90f38e0fdd0caf1fc87ea0
David Son <iphone_1st@naver.com>
2021-11-24 13:00:13 +0100

    Go rutine check
    순차 실행이지만, 확인 해볼 것.

--- Untitled-1.go
+++ Untitled-1.go
@@ -1,1 +1,26 @@
-fmt.Printf("", var)
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	done := make(chan bool)
+
+	go func() {
+		for i := 0; i < 4; i++ {
+			done <- true
+			fmt.Println("고루틴 : ", i)
+
+		}
+	}()
+
+	for i := 0; i < 4; i++ {
+		<-done
+
+		fmt.Println("메인 함수 : ", i)
+
+		time.Sleep(time.Second)
+	}
+}
